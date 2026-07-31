e3b5c0f65dde1879a9a081ff2e227148f10a5b73
Pronob Das <251-15-155@diu.edu.bd>
2026-07-31 19:28:11 +0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+=========================================
+DailyOS
+Version 1
+
+Main JavaScript Entry Point
+=========================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("DailyOS Initialized.");
+
+});
